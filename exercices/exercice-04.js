9cbd3464117340103d225eb20b5e41e7657cc8c8
sahmi houssam <sahmihoussam1@gmail.com>
2026-09-09 18:28:55 +0200

    second commit

--- exercices/exercice-04.js
+++ exercices/exercice-04.js
@@ -18,3 +18,8 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+let somme=0;
+for (let i=1 ; i <= 50 ; i++) {
+somme = somme+i;
+}
+console.log(somme);
